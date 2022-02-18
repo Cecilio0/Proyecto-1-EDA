9eb41b9fb4a416e906bdb90ea7dd6fcb76d9e63c
simongonzalez47 <99984830+simongonzalez47@users.noreply.github.com>
2022-02-18 16:14:58 -0500

    Cliente/Persona

---
+++ Taller/src/taller/Cliente.java
@@ -0,0 +1,12 @@
+package taller;
+
+import java.util.*;
+
+public class Cliente extends Persona {
+    private Vehiculos[] vehiculos;
+    private double deuda;
+    private Date fechaRegistro;
+
+
+
+}
