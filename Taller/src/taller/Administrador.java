8e0c4d6be4d9c678afe1a01c58f54b4232a88f44
simongonzalez47 <99984830+simongonzalez47@users.noreply.github.com>
2022-02-21 14:21:28 -0500

    Update Administrador.java

--- Taller/src/taller/Administrador.java
+++ Taller/src/taller/Administrador.java
@@ -1,13 +1,19 @@
 package taller;
 
+import java.util.*;
+
 public class Administrador {
 	private Cliente[] clientes;
 	private Mecanico[] mecanicos;
-	
-	public Administrador() {
-		
-	}
-	
-	
 
+	public Administrador() {
+
+	}
+
+	public void addCliente(String nombre, String cedula, Date fechaNacimiento, String direccion, String telefono, Vehiculo[] vehiculos, String correo, Date fechaRegistro) {
+		Cliente c = new Cliente(nombre, cedula, fechaNacimiento, direccion, telefono, vehiculos, correo, fechaRegistro);
+		clientes[clientes.length] = c;
+	}
+
+	
 }
