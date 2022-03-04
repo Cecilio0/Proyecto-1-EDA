8da24d5a78907b4fc7b2377cbef0c95f42d9a00d
DanielRestrepo53 <daniel.restrepo53@eia.edu.co>
2022-03-04 14:24:01 -0500

    Medio Prueba Ficheros

--- Taller/src/taller/Main.java
+++ Taller/src/taller/Main.java
@@ -1,12 +1,86 @@
 package taller;
 
+import java.io.*;
 import java.util.*;
 
 
 public class Main {
+	//considerar poner todo lo de los ficheros en su propia clase
+	private static File[] ficheros;
 
-	public static void main(String[] args) {
+	public static File buscarFichero(String nombreFichero) throws FileNotFoundException {
+		int i = -1;
+		while (++i < ficheros.length && ficheros[i] != null && !ficheros[i].getName().equalsIgnoreCase(nombreFichero));
 		
+		if (i != ficheros.length+1) {
+			return ficheros[i-1];
+		}
+		throw new FileNotFoundException();
+	}
+	
+	public static void cargarFicheros(String nombreFichero) throws IOException {//crea o carga los ficheros
+		for (int i = 0; i < ficheros.length; i++) {
+			if (!ficheros[i].exists()) {
+				ficheros[i].createNewFile();
+			}
+		}
+		//cargar toda la informacion al programa (Clientes, Mecanicos, precios)
+	}
+	
+	public static void addInformacionFichero(String nombreFichero) throws FileNotFoundException {
+		try {
+			buscarFichero(nombreFichero);
+		} catch (FileNotFoundException e) {
+			throw e;
+		}
+	}
+	
+	public static void borrarDatoFichero(String nombreFichero, String dato) throws IOException {//borra una linea de un fichero pero para hacerlo se debe hacer un dato sin repetir eg: cedula
+		File archivoO = buscarFichero(nombreFichero);
+		File archivoT = new File("archivoT.txt");
+		
+		BufferedReader reader = new BufferedReader(new FileReader(archivoO));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(archivoT));
+		String aux;
+		
+		while ((aux = reader.readLine()) != null) {
+			if (aux.contains(dato)) {
+				continue;
+			}
+			writer.write(aux + "\n");
+		}
+		reader.close();
+		writer.close();
+		archivoO.delete();
+		archivoT.renameTo(archivoO);
+			
+		
+	}
+	
+	public static void actualizarDatoFichero(String nombreFichero, String dato, String nuevoDato) throws IOException{		File archivoO = buscarFichero(nombreFichero);
+		File archivoO1 = buscarFichero(nombreFichero);
+		File archivoT = new File("archivoT.txt");
+		
+		BufferedReader reader = new BufferedReader(new FileReader(archivoO1));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(archivoT));
+		String aux;
+		
+		while ((aux = reader.readLine()) != null) {
+			if (aux.contains(dato)) {
+				continue;
+			}
+			writer.write(aux + "\n");
+		}
+		reader.close();
+		writer.close();
+		archivoO1.delete();
+		archivoT.renameTo(archivoO1);
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
 		//hacer ficheros aqui para llamarlos cada vez que se inicie el codigo
 		
 		Scanner sc = new Scanner (System.in);
